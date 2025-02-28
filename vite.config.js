33783c889a2a715c2bbcb7f389178c8137a0b48c
Zoe K <zfk20@email.vccs.edu>
2025-02-28 10:17:41 -0500

    fixed base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-    base: "/Web-Project/",
+    base: "/Web-Page/",
     build: { 
         outDir: "docs",
         rollupOptions: {
