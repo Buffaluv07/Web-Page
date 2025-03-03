f14e598067d12cedd4ef5e7fb09f0b5bd963298d
Zoe K <zfk20@email.vccs.edu>
2025-03-03 12:07:52 -0500

    added more pages and work ive done

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
                 lineup: resolve(__dirname, 'lineup/index.html'),
                 location: resolve(__dirname, 'location/index.html'),
                 projects : resolve(__dirname, 'projects/index.html'),
-
+                skyline: resolve(__dirname, 'skyline/index.html'),
+                FerrisWheel: resolve(__dirname, 'FerrisWheel/index.html'),
             }
         }
     }
