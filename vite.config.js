6203c6c85d985385d46dff5aeef58e343a45c8c7
Zoe K <zfk20@email.vccs.edu>
2025-04-11 13:38:12 -0400

    new file

--- vite.config.js
+++ vite.config.js
@@ -17,9 +17,9 @@ export default defineConfig({
                 Roman: resolve(__dirname, 'Roman/index.html'),
                 Cat: resolve(__dirname, 'Cat/index.html'),
                 RockPaper: resolve(__dirname, 'RockPaper/index.html'),
-                ToDoApp: resolve(__dirname, 'ToDo-App/index.html'),
                 Bus: resolve(__dirname, 'Bus/index.html'),
                 Cash: resolve(__dirname, 'Cash/index.html'),
+                Notes: resolve(__dirname, 'Notes-App/index.html'),
             }
         },
     }
